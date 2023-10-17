8dd56983b6f0358786bbd8789cb8cb14332ec6e2
Kenneth Hedman <github@vitberget.se>
2023-10-17 13:34:12 +0200

    refactoring

--- src/changes.rs
+++ src/changes.rs
@@ -16,13 +16,18 @@ impl RepoChanges {
     fn both_true(&self) -> bool {
         self.worktree && self.index
     }
+
+    fn export_true_false(&self) {
+        println!("export GIT_PROMPT_INDEX_CHANGED={}", self.index);
+        println!("export GIT_PROMPT_WORKTREE_CHANGED={}", self.worktree);
+    }
 }
 
 pub(crate) fn changes_index_and_worktree(repo: &Repository) {
     let mut status_options = StatusOptions::new();
     status_options.include_untracked(true);
 
-    let repo_changes = repo.statuses(Some(&mut status_options))
+    repo.statuses(Some(&mut status_options))
         .iter()
         .fold(RepoChanges::new(), |acc, statuses| {
             if acc.both_true() {
@@ -32,9 +37,7 @@ pub(crate) fn changes_index_and_worktree(repo: &Repository) {
                     .filter(|entry| entry.status() != Status::CURRENT)
                     .fold(acc, |acc, entry| update_repo_changes(acc, entry))
             }
-        });
-
-    export_true_false(&repo_changes);
+        }).export_true_false();
 }
 
 fn update_repo_changes(repo_changes: RepoChanges, entry: StatusEntry<'_>) -> RepoChanges {
@@ -59,9 +62,4 @@ fn update_repo_changes(repo_changes: RepoChanges, entry: StatusEntry<'_>) -> Rep
 
         RepoChanges { index, worktree }
     }  
-}
-
-fn export_true_false(repo_changes: &RepoChanges) {
-    println!("export GIT_PROMPT_INDEX_CHANGED={}", repo_changes.index);
-    println!("export GIT_PROMPT_WORKTREE_CHANGED={}", repo_changes.worktree);
 }
